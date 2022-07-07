1bcccc070abfd7b1593e2ad54f1f7562c4334ed1
Mystiick <Mystiick@outlook.com>
2022-07-06 19:54:06 -0500

    Add cache to keep track of current page number

---
+++ MystiickWeb.Client/Services/CacheService.cs
@@ -0,0 +1,41 @@
+﻿namespace MystiickWeb.Client.Services;
+
+public class CacheService
+{
+    private Dictionary<string, object> _cache = new();
+
+    public void Save<T>(string key, T value) where T : notnull
+    {
+        if (_cache.ContainsKey(key))
+        {
+            _cache[key] = value;
+        }
+        else
+        {
+            _cache.Add(key, value);
+        }
+    }
+
+    public T Load<T>(string key)
+    {
+        return (T)_cache[key];
+    }
+
+    public bool Delete(string key)
+    {
+        if (_cache.ContainsKey(key))
+        {
+            _cache.Remove(key);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool Contains(string key)
+    {
+        return _cache.ContainsKey(key);
+    }
+}
