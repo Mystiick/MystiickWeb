046479ec0a62a902be09c0b4c99118d6e49a1335
Mystiick <Mystiick@outlook.com>
2022-08-27 16:24:29 -0500

    Add missed files

---
+++ MystiickWeb.Shared/Models/Response.cs
@@ -0,0 +1,19 @@
+﻿namespace MystiickWeb.Shared.Models;
+
+public class Response<T> : Response
+{
+    public T? Value { get; set; }
+}
+
+public class Response
+{
+    public bool Success { get; set; } = false;
+    public string Message { get; set; } = string.Empty;
+
+    public Response() { }
+    public Response(Response<Response> response)
+    {
+        Success = response.Success;
+        Message = response.Message;
+    }
+}
