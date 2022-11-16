726690b5edd55f3e814d1dd1e3c4b806ea6d9b87
Mystiick <Mystiick@outlook.com>
2022-11-15 22:23:00 -0600

    Implement IUserRoleStore<User>

--- MystiickWeb.Clients/Identity/MystiickUserStore.UserRoleStore.cs
+++ MystiickWeb.Clients/Identity/MystiickUserStore.UserRoleStore.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Dapper;
+
+using Microsoft.AspNetCore.Identity;
+
+using MySql.Data.MySqlClient;
 
 using MystiickWeb.Shared.Models.User;
 
@@ -7,30 +11,59 @@ namespace MystiickWeb.Clients.Identity
 {
     public partial class MystiickUserStore : IUserRoleStore<User>
     {
-        public Task AddToRoleAsync(User user, string roleName, CancellationToken cancellationToken)
+        public async Task AddToRoleAsync(User user, string roleName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            using var connection = new MySqlConnection(_connections.UserDatabase);
+            await connection.OpenAsync(cancellationToken);
+
+            await connection.ExecuteAsync(
+                "insert into UserRole (ID, UserID, Role, Created, Updated) values (@ID, @UserID, @Role, @Created, @Created)",
+                new
+                {
+                    ID = Guid.NewGuid(),
+                    UserID = user.ID,
+                    Role = roleName,
+                    Created = DateTime.Now
+                }
+            );
         }
 
-        public Task RemoveFromRoleAsync(User user, string roleName, CancellationToken cancellationToken)
+        public async Task RemoveFromRoleAsync(User user, string roleName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            using var connection = new MySqlConnection(_connections.UserDatabase);
+            await connection.OpenAsync(cancellationToken);
+
+            await connection.ExecuteAsync(
+                "delete from UserRole where UserID = @UserID AND Role = @Role",
+                new
+                {
+                    UserID = user.ID,
+                    Role = roleName
+                }
+            );
         }
 
-        public Task<IList<string>> GetRolesAsync(User user, CancellationToken cancellationToken)
+        public async Task<IList<string>> GetRolesAsync(User user, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            using var connection = new MySqlConnection(_connections.UserDatabase);
+            await connection.OpenAsync(cancellationToken);
+
+            var output = await connection.QueryAsync<string>(
+                "select Role from UserRole where UserID = @UserID",
+                new { UserID = user.ID }
+            );
+
+            return output.ToList();
         }
 
-        public Task<bool> IsInRoleAsync(User user, string roleName, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<bool> IsInRoleAsync(User user, string roleName, CancellationToken cancellationToken) => (await GetRolesAsync(user, cancellationToken)).Contains(roleName);
 
-        public Task<IList<User>> GetUsersInRoleAsync(string roleName, CancellationToken cancellationToken)
+        public async Task<IList<User>> GetUsersInRoleAsync(string roleName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
-        }
+            using var connection = new MySqlConnection(_connections.UserDatabase);
+            await connection.OpenAsync(cancellationToken);
 
+            return (await connection.QueryAsync<User>("select * from User where ID IN (select UserID from UserRole where Role = @Role)")).ToList();
+        }
     }
 }
